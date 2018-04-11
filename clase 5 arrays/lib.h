74197c58b5d859aadb9e1bac67185e9342e35475
ivan <you@example.com>
2018-04-11 00:11:48 -0300

    funciones

--- clase 5 arrays/lib.h
+++ clase 5 arrays/lib.h
@@ -2,6 +2,11 @@
 #define LIB_H_INCLUDED
 #endif // LIB_H_INCLUDED
 
+int sumaNumeros (int numero1, int numero2, float* resultadoSuma);
+int restaNumeros(int numero1, int numero2, float* resultadoResta);
+int divisionNumeros(int numero1, int numero2, float* resultadoDivision);
+int multiplicacionNumeros(int numero1, int numero2, float*resultadoMultiplicacion);
+int factorialNumero1(int numero1, float* resultadoFactorial);
 int getInt(int* resultado, char*mensaje, char*mensajeError, int maximo, int minimo, int reintentos);
 int getFloat(float *resultado, char* mensaje, char* mensajeError, float maximo, float minimo, int reintentos);
 int getChar(char *resultado, char* mensaje, char* mensajeError, char maximo, char minimo, int reintentos);
